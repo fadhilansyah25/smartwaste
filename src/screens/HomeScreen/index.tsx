1f461d294e3807e1f028aa932daa62d1a508bdca
fadhilansyah25 <fadhilansyah25@gmail.com>
2023-04-09 15:54:41 +0700

    UI: create mainbanner at homescreen

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -4,11 +4,14 @@ import {
   TouchableOpacity,
   Text,
   View,
+  Image,
 } from 'react-native';
 import {style} from './style';
 import auth from '@react-native-firebase/auth';
 import {RootStackProps} from '../../App';
 import {colors} from '../../const/colors';
+import LinearGradient from 'react-native-linear-gradient';
+import CustomButton from '../../component/Button';
 
 const HomeScreen = ({navigation}: RootStackProps) => {
   const handleSignOut = () => {
@@ -21,44 +24,72 @@ const HomeScreen = ({navigation}: RootStackProps) => {
         showsVerticalScrollIndicator={false}
         keyboardShouldPersistTaps="handled">
         {/* Greetings bar and avatar */}
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            marginTop: 14,
-          }}>
+        <View style={style.greetingsBarContainer}>
           <Text style={{color: colors.T600, fontWeight: '600', fontSize: 16}}>
             Selamat Datang!
           </Text>
           <View style={{flexDirection: 'row'}}>
             <View style={{marginRight: 10}}>
-              <Text
-                style={{
-                  fontSize: 14,
-                  fontWeight: '700',
-                  color: colors.T600,
-                }}>
+              <Text style={style.displayName}>
                 {auth().currentUser?.displayName}
               </Text>
               <Text style={{fontSize: 12, lineHeight: 12}}>volunter</Text>
             </View>
-            <View
-              style={{
-                height: 40,
-                width: 40,
-                borderRadius: 40,
-                backgroundColor: 'gray',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
+            <View style={style.avatarBagde}>
               <Text style={{textAlign: 'center'}}>FA</Text>
             </View>
           </View>
         </View>
 
         {/* Main Banner */}
-        <View></View>
+        <View style={style.mainBannerContainer}>
+          <LinearGradient
+            colors={['#4CB8C4', '#3CD3AD']}
+            style={style.mainBannerGradient}
+            start={{x: 0, y: 0}}
+            end={{x: 0.7, y: 0}}>
+            {/* Banner text copy */}
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                paddingVertical: 14,
+                paddingHorizontal: 16,
+              }}>
+              <View style={{flex: 1}}>
+                <Text
+                  style={{
+                    color: colors.white,
+                    fontSize: 18,
+                    fontWeight: '700',
+                  }}>
+                  Send Your Waste
+                </Text>
+                <Text style={{color: colors.white, fontSize: 12}}>
+                  Yuk buat transaksi pertamamu! Packing kemasan bekas Anda dan
+                  kirim ke mitra Smart Waste
+                </Text>
+              </View>
+              <View style={{alignItems: 'center'}}>
+                <Image
+                  source={require('../../assets/image/illustration-5.png')}
+                  style={style.illustration}
+                />
+              </View>
+            </View>
+
+            {/* Banner Button */}
+            <CustomButton
+              style={{
+                backgroundColor: colors.O800,
+                marginVertical: 14,
+                marginHorizontal: 16,
+              }}
+              label="Kirim Sekarang"
+              type="fill"
+            />
+          </LinearGradient>
+        </View>
 
         <TouchableOpacity onPress={handleSignOut}>
           <Text>Log Out</Text>
--- src/screens/HomeScreen/style.ts
+++ src/screens/HomeScreen/style.ts
@@ -6,4 +6,33 @@ export const style = StyleSheet.create({
     paddingHorizontal: 20,
     backgroundColor: colors.white,
   },
+  greetingsBarContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 14,
+  },
+  avatarBagde: {
+    height: 40,
+    width: 40,
+    borderRadius: 40,
+    backgroundColor: 'gray',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  displayName: {
+    fontSize: 14,
+    fontWeight: '700',
+    color: colors.T600,
+  },
+  mainBannerContainer: {
+    marginTop: 20,
+  },
+  mainBannerGradient: {
+    borderRadius: 10,
+    width: '100%',
+  },
+  illustration: {
+    resizeMode: 'contain',
+  },
 });
