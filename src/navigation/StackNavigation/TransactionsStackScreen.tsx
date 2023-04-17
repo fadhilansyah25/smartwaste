17404c922f9e4bfedb82382b2f35fed2def4d4b7
fadhilansyah25 <fadhilansyah25@gmail.com>
2023-04-17 20:44:15 +0700

    add: install react native mapbox

--- src/navigation/StackNavigation/TransactionsStackScreen.tsx
+++ src/navigation/StackNavigation/TransactionsStackScreen.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Text, View, TouchableOpacity} from 'react-native';
 import {
-  NativeStackNavigationProp,
   NativeStackScreenProps,
   createNativeStackNavigator,
 } from '@react-navigation/native-stack';
