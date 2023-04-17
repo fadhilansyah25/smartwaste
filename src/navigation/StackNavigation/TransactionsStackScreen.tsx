c4ce74b49319284b72d8c2fe17a9ac32179b35eb
fadhilansyah25 <fadhilansyah25@gmail.com>
2023-04-17 15:22:09 +0700

    create: pick point map screen

--- src/navigation/StackNavigation/TransactionsStackScreen.tsx
+++ src/navigation/StackNavigation/TransactionsStackScreen.tsx
@@ -1,29 +1,35 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, TouchableOpacity} from 'react-native';
 import {
+  NativeStackNavigationProp,
   NativeStackScreenProps,
   createNativeStackNavigator,
 } from '@react-navigation/native-stack';
+import {useNavigation} from '@react-navigation/native';
 import {CustomHeader} from '@src/component';
 import {colors} from '@src/const/colors';
 import SearchMitraScreen from '@src/screens/SearchMitraScreen';
+import PickPointScreen from '@src/screens/PickPointScreen';
 import auth from '@react-native-firebase/auth';
 
-export type HomeStackParamaterList = {
-  Home: undefined;
+export type TransactionStackParamaterList = {
+  SearchMitra: undefined;
+  PickPoint: undefined;
 };
 
-export type HomeStackProps = NativeStackScreenProps<HomeStackParamaterList>;
+export type TransactionStackProps =
+  NativeStackScreenProps<TransactionStackParamaterList>;
 
-const HomeStack = createNativeStackNavigator<HomeStackParamaterList>();
+const TransactionStack =
+  createNativeStackNavigator<TransactionStackParamaterList>();
 
 const TransactionsStackScreen = () => {
   return (
-    <HomeStack.Navigator
-      initialRouteName="Home"
+    <TransactionStack.Navigator
+      initialRouteName="SearchMitra"
       screenOptions={{headerShown: false}}>
-      <HomeStack.Screen
-        name="Home"
+      <TransactionStack.Screen
+        name="SearchMitra"
         component={SearchMitraScreen}
         options={{
           headerShown: true,
@@ -39,21 +45,43 @@ const TransactionsStackScreen = () => {
           ),
         }}
       />
-    </HomeStack.Navigator>
+      <TransactionStack.Screen
+        name="PickPoint"
+        component={PickPointScreen}
+        options={{
+          headerShown: true,
+          header: props => (
+            <CustomHeader
+              headerProps={props}
+              backButton
+              logo={false}
+              headerCenter={<CenterHeader />}
+              elevation={0}
+            />
+          ),
+        }}
+      />
+    </TransactionStack.Navigator>
   );
 };
 
 const RightHeader = () => {
+  const navigation = useNavigation<TransactionStackProps['navigation']>();
+
   return (
-    <Text
-      style={{
-        flex: 1,
-        textAlign: 'right',
-        fontWeight: '700',
-        color: colors.T500,
-      }}>
-      Ganti
-    </Text>
+    <TouchableOpacity
+      style={{flex: 1}}
+      activeOpacity={0.9}
+      onPress={() => navigation.navigate('PickPoint')}>
+      <Text
+        style={{
+          textAlign: 'right',
+          fontWeight: '700',
+          color: colors.T500,
+        }}>
+        Ganti
+      </Text>
+    </TouchableOpacity>
   );
 };
 
@@ -61,7 +89,8 @@ const CenterHeader = () => {
   return (
     <View style={{marginLeft: 20}}>
       <Text style={{fontSize: 12, fontWeight: '600', color: colors.T500}}>
-        <Text style={{color: colors.N400}}>Lokasi Anda</Text>{'  '}
+        <Text style={{color: colors.N400}}>Lokasi Anda</Text>
+        {'  '}
         {auth().currentUser?.displayName}
       </Text>
       <Text style={{fontSize: 12}}>{auth().currentUser?.displayName}</Text>
---
+++ src/screens/PickPointScreen/index.tsx
@@ -0,0 +1,11 @@
+import {SafeAreaView, Text} from 'react-native';
+
+function PickPointScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Pick Poin</Text>
+    </SafeAreaView>
+  );
+}
+
+export default PickPointScreen;
