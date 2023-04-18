9f1e08cdf61a0ce779de592680fb25000a0005bf
fadhilansyah25 <fadhilansyah25@gmail.com>
2023-04-18 17:24:38 +0700

    add: testing maps feature

--- src/screens/PickPointScreen/index.tsx
+++ src/screens/PickPointScreen/index.tsx
@@ -1,9 +1,18 @@
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Text, View} from 'react-native';
+import {style} from './style';
+import Mapbox from '@rnmapbox/maps';
+
+Mapbox.setAccessToken(
+  'pk.eyJ1IjoiZmFkaGlsYW5zeWFoMjUiLCJhIjoiY2t6MTJkdXU4MGZ4bjJ2cWZyYmxpcjRkeiJ9.M7A_UY3v9F8yFvyknwU5_g',
+);
 
 function PickPointScreen() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={style.screenContainer}>
       <Text>Pick Poin</Text>
+      <View style={style.container}>
+        <Mapbox.MapView style={style.map} />
+      </View>
     </SafeAreaView>
   );
 }
