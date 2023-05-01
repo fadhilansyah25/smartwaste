85e68369c4afdcfdfe0ccb8eda804d51cd193092
fadhilansyah25 <fadhilansyah25@gmail.com>
2023-05-01 20:30:21 +0700

    update: update reverse geocode api and finishing UI

--- src/screens/PickPointScreen/index.tsx
+++ src/screens/PickPointScreen/index.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import {PermissionsAndroid, SafeAreaView, View} from 'react-native';
+import {
+  PermissionsAndroid,
+  SafeAreaView,
+  View,
+  TextInput,
+  Text,
+} from 'react-native';
 import {style} from './style';
 import MapboxGL from '@rnmapbox/maps';
 import {MAP_BOX_TOKEN_ACCESS} from '@env';
 import Geolocation from 'react-native-geolocation-service';
 import MarkerSvg from '@src/assets/svg/map-marker.svg';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {colors} from '@src/const/colors';
+import {LocationAddress} from '@src/types/location';
+import {CustomButton} from '@src/component';
 
 MapboxGL.setAccessToken(MAP_BOX_TOKEN_ACCESS);
 
 function PickPointScreen() {
   const [location, setLocation] = React.useState<number[]>();
-  const [addrLocation, setAddrLocation] = React.useState<any>(null);
+  const [addrLocation, setAddrLocation] = React.useState<
+    LocationAddress | undefined
+  >();
 
   const getLocation = () => {
     const result = requestLocationPermission();
@@ -34,7 +46,8 @@ function PickPointScreen() {
     position: MapboxGL.MapState['properties']['center'],
   ) => {
     const response = await fetch(
-      `https://api.mapbox.com/geocoding/v5/mapbox.places/${position[0]},${position[1]}.json?access_token=${MAP_BOX_TOKEN_ACCESS}`,
+      `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${position[1]}&lon=${position[0]}&addressdetails=1&limit=1`,
+      // `https://api.mapbox.com/geocoding/v5/mapbox.places/${position[0]},${position[1]}.json?access_token=${MAP_BOX_TOKEN_ACCESS}`,
     );
     const data = await response.json();
     setAddrLocation(data);
@@ -52,8 +65,11 @@ function PickPointScreen() {
     reverseGeocoding(state.properties.center);
   };
 
+  console.log(addrLocation);
+
   return (
     <SafeAreaView style={style.screenContainer}>
+      {/* MapView */}
       <View style={style.container}>
         {location ? (
           <View style={{flex: 1, position: 'relative'}}>
@@ -87,6 +103,46 @@ function PickPointScreen() {
             </MapboxGL.MapView>
           </View>
         ) : null}
+        {/* Search Location */}
+        <View>
+          <View
+            style={{
+              paddingVertical: 12,
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                padding: 8,
+                columnGap: 10,
+                borderColor: colors.T500,
+                borderWidth: 1,
+                borderRadius: 4,
+                backgroundColor: colors.N100,
+                flex: 1,
+              }}>
+              <Icon name="ios-search" size={24} />
+              <TextInput
+                editable={false}
+                placeholder="Cari Lokasi"
+                style={{padding: 0, flex: 1}}
+              />
+            </View>
+          </View>
+        </View>
+      </View>
+
+      {/* Location Address */}
+      <View style={{marginHorizontal: 20, marginBottom: 20}}>
+        <Text style={{fontSize: 12, marginBottom: 8}}>Titik Lokasi Anda</Text>
+        <Text>{addrLocation?.display_name}</Text>
+      </View>
+
+      {/* Button Save Location */}
+      <View style={{marginHorizontal: 20, marginBottom: 20}}>
+        <CustomButton label="Pilih Lokasi" type="fill" />
       </View>
     </SafeAreaView>
   );
