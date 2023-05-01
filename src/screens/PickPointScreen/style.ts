991ee22f4d0968dbada0b29e8251240460a2be1c
fadhilansyah25 <fadhilansyah25@gmail.com>
2023-05-01 20:30:45 +0700

    adjustiing: style from pick point screen

--- src/screens/PickPointScreen/style.ts
+++ src/screens/PickPointScreen/style.ts
@@ -5,11 +5,12 @@ export const style = StyleSheet.create({
   screenContainer: {
     flex: 1,
     backgroundColor: colors.white,
+    justifyContent: 'space-between',
   },
   container: {
     flex: 1,
     marginHorizontal: 20,
-    marginVertical: 20,
+    marginTop: 20,
     position: 'relative',
   },
   map: {
