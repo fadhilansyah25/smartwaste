2cc0cbec68bc72f19529129dac07afbb0b6d2225
fadhilansyah25 <fadhilansyah25@gmail.com>
2023-05-04 21:06:58 +0700

    update: add search location function with nominatim API

--- src/screens/SearchLocationScreen/index.tsx
+++ src/screens/SearchLocationScreen/index.tsx
@@ -5,9 +5,34 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import {colors} from '@src/const/colors';
 import {useNavigation} from '@react-navigation/native';
 import {TransactionStackProps} from '@src/navigation/StackNavigation/TransactionsStackScreen';
+import {SearchLOC} from '@src/types/searchLoc';
 
 const SearchLocationScreen = () => {
   const navigation = useNavigation<TransactionStackProps['navigation']>();
+  const [searchText, setSearchText] = React.useState('');
+  const [searchResults, setSearchResults] = React.useState<SearchLOC[]>([]);
+
+  const searchLocation = React.useCallback(async () => {
+    try {
+      const response = await fetch(
+        `https://nominatim.openstreetmap.org/search?q=${searchText}&format=json`,
+      );
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.error(error);
+    }
+  }, [searchText]);
+
+  React.useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (searchText.length > 2) {
+        searchLocation();
+      }
+    }, 1000);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [searchText]);
 
   return (
     <SafeAreaView style={style.screenContainer}>
@@ -17,6 +42,8 @@ const SearchLocationScreen = () => {
         </TouchableOpacity>
         <View style={style.searchBarContent}>
           <TextInput
+            value={searchText}
+            onChangeText={text => setSearchText(text)}
             placeholder="Cari Lokasi"
             style={{padding: 0, paddingLeft: 10, flex: 1}}
           />
