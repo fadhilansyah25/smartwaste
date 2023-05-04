d11ffc2af3f14a37717d28f831c1f42a2587a83c
fadhilansyah25 <fadhilansyah25@gmail.com>
2023-05-04 20:20:37 +0700

    add: search bar component to Search Location Screen

--- src/screens/SearchLocationScreen/index.tsx
+++ src/screens/SearchLocationScreen/index.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
-import {View, SafeAreaView} from 'react-native';
+import {View, SafeAreaView, TextInput, TouchableOpacity} from 'react-native';
 import {style} from './style';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {colors} from '@src/const/colors';
+import {useNavigation} from '@react-navigation/native';
+import {TransactionStackProps} from '@src/navigation/StackNavigation/TransactionsStackScreen';
 
 const SearchLocationScreen = () => {
-  return <SafeAreaView style={style.screenContainer}></SafeAreaView>;
+  const navigation = useNavigation<TransactionStackProps['navigation']>();
+
+  return (
+    <SafeAreaView style={style.screenContainer}>
+      <View style={style.searchBarContainer}>
+        <TouchableOpacity onPress={() => navigation.navigate('PickPoint')}>
+          <Icon name="ios-chevron-back" size={36} color={colors.T500} />
+        </TouchableOpacity>
+        <View style={style.searchBarContent}>
+          <TextInput
+            placeholder="Cari Lokasi"
+            style={{padding: 0, paddingLeft: 10, flex: 1}}
+          />
+          <Icon name="ios-search" size={24} />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default SearchLocationScreen;
