4244db93188b5fc2f341d716bd519c5bf8e26783
fadhilansyah25 <fadhilansyah25@gmail.com>
2023-05-05 20:20:12 +0700

    create: init Mitra Detail Screen

---
+++ src/screens/MitraDetailScreen/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import {style} from './style';
+
+const MitraDetailScreen = () => {
+  return <SafeAreaView style={style.screenContainer}></SafeAreaView>;
+};
+
+export default MitraDetailScreen;
---
+++ src/screens/MitraDetailScreen/style.ts
@@ -0,0 +1,10 @@
+import {colors} from '@src/const/colors';
+import {StyleSheet} from 'react-native';
+
+export const style = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    backgroundColor: colors.white,
+    justifyContent: 'space-between',
+  },
+});
