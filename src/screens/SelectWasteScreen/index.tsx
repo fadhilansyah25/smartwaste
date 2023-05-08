f78b8afb6bc7f9a96fab0230d46f8d5e87f09834
fadhilansyah25 <fadhilansyah25@gmail.com>
2023-05-08 16:38:25 +0700

    intialize: SelectWasteScreen

---
+++ src/screens/SelectWasteScreen/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import {style} from './style';
+
+const SelectWasteScreen = () => {
+  return <SafeAreaView style={style.screenContainer}></SafeAreaView>;
+};
+
+export default SelectWasteScreen;
