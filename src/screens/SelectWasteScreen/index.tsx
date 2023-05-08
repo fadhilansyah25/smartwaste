91922f342b478cdae0bfa6bca2f1e93784cc582f
fadhilansyah25 <fadhilansyah25@gmail.com>
2023-05-08 18:17:23 +0700

    add: install package @react-native-community/checkbox

--- src/screens/SelectWasteScreen/index.tsx
+++ src/screens/SelectWasteScreen/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 import {style} from './style';
+import {TransactionContext} from '@src/store/context/TransactionContext';
 
 const SelectWasteScreen = () => {
+  const {state, dispatch} = React.useContext(TransactionContext);
+
   return <SafeAreaView style={style.screenContainer}></SafeAreaView>;
 };
 
